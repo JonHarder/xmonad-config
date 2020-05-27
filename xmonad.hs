ff99730e0897b5fcb17c176267535f4b9f4fe51c
Jon Harder <jonharder6@gmail.com>
2020-05-27 04:01:22 -0500

    multi-monitor display for xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,7 @@
 import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicBars
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig (additionalKeys)
@@ -11,14 +12,14 @@ term :: String
 term = "urxvt"
 
 
-myLogHook :: Handle -> X ()
-myLogHook bar = dynamicLogWithPP xmobarPP
-  { ppOutput = hPutStrLn bar
+myLogHook :: [Handle] -> X ()
+myLogHook bars = dynamicLogWithPP xmobarPP
+  { ppOutput = \x -> mapM_ (\bar -> hPutStrLn bar x) bars
   , ppTitle = xmobarColor "green" "" . shorten 50
   }
 
 
-myLayoutHook = smartSpacing 10 $ avoidStruts $ layoutHook defaultConfig
+myLayoutHook = smartSpacing 10 $ avoidStruts $ layoutHook def
 
 
 myKeyMap :: [((KeyMask, KeySym), X ())]
@@ -28,8 +29,8 @@ myKeyMap =
   ]
 
 
-defaults bar = def
-  { logHook = myLogHook bar
+defaults bars = def
+  { logHook = myLogHook bars
   , layoutHook = myLayoutHook
   , manageHook = manageDocks <+> manageHook def
   , handleEventHook = handleEventHook def <+> docksEventHook
@@ -38,5 +39,11 @@ defaults bar = def
   } `additionalKeys` myKeyMap
 
 
+startBar :: MonadIO m => Int -> m Handle
+startBar i = spawnPipe $ "xmobar -x " ++ show i
+
+-- TODO: look into XMonad.Hooks.DynamicBars to handle per display bar
 main :: IO ()
-main = xmonad =<< defaults <$> spawnPipe "xmobar"
+main = do
+  bars <- mapM startBar [0..1]
+  xmonad $ defaults bars
